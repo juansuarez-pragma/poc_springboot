813046fe9093b68cc9bc14d2b5ecb9486cd8b781
Juan Carlos Suarez Marin <ojs02431@MINMAC04-T.local>
2024-09-04 16:42:31 -0500

    initial project

---
+++ src/main/java/pocSpringBoot/controllers/HelloWorldRestController.java
@@ -0,0 +1,13 @@
+package pocSpringBoot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, world";
+    }
+}
